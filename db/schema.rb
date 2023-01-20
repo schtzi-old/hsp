99517f2e9ddc6f6c9c5d5d483230956e0eea1b48
Jan Schuetz <janalexschuetz@gmail.com>
2023-01-20 13:24:11 +1300

    Added attributes to sport

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_24_025255) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_20_002317) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,6 +30,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_24_025255) do
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "stamina"
+    t.integer "power"
+    t.integer "effort"
+    t.integer "coordination"
+    t.integer "physical_contact"
   end
 
   create_table "users", force: :cascade do |t|
