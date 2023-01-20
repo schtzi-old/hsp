efdff0cb7c4645dddcdd40a07f76cfe8db7617be
Jan Schuetz <janalexschuetz@gmail.com>
2023-01-20 14:07:37 +1300

    implemented tests and validations

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,39 @@
 names = ["Ski und Snowboard", "Fußball", 'Triathlon', "Rennrad"]
-sports = {
-  'Ski und Snowboard' => "Bester Sport der Welt und dementsprechend sollte jeder diese Sportart buchen - achja, ich spreche von Ski",
-  'Fußball' => 'Deutschlands populärster Sport'
-}
+sports = [
+  { name: 'Schach',
+    description: 'Schach ist eines der bekanntesten Brettspiele der Welt und die Meisten haben es schon einmal irgendwann kennengelernt und ausprobiert. Sowohl längerfristige Strategie, als auch kurzfristige taktische Manöver gilt es zu finden und richtig umzusetzen, um den gegnerischen König Schachmatt zu setzen.     Ob Sport, Spiel, Wissenschaft oder Kunst, das ?Spiel der Könige? trainiert sehr viele mentale Fähigkeiten: Gedächtnis, Konzentrationsfähigkeit, Vorausdenken, Denkgeschwindigkeit etc. Dennoch ist es uns wichtig, dass der Spaß- und Spielaspekt nicht verloren geht. So wird jedes Training viel gespielt und man lernt schnell tolle Leute kennen.
+                  Selbst nach einem langen Uni-Tag bietet Schach eine gute Möglichkeit sein Gehirn auf andere Art und Weise zu benutzen und damit den Kopf frei zu kriegen.
+                  In unserem Kurs sind alle Spielstärken, vom Neuling bis zum Vereins-Spieler herzlich willkommen und können individuell trainiert werden.
+                  Wenn du Interesse an einer Teilnahme an den deutschen Hochschulmeisterschaften hast, sprich uns gerne direkt an.',
+    stamina: 2,
+    power: 2,
+    effort: 2,
+    coordination: 2,
+    physical_contact: 2 },
+  { name: 'Quidditch',
+    description: 'Quidditch ist eine gemischtgeschlechtliche Vollkontaktsportart, bei der maximal 3 Personen eines Genders gleichzeitig auf dem Feld stehen, und die ihren Ursprung in den Harry Potter Büchern hat. Auch wenn der Sport heute fast nichts mehr mit der Vorlage zu tun hat, so sind die Regeln noch sehr ähnlich: Die Chaser versuchen den Quaffel durch die gegnerischen Ringe zu werfen, um zu punkten. Die Beater versuchen sie mit den Bludgern abzuwerfen und nach 20 Minuten kommen die Seeker auf das Feld, um den Schnatz zu fangen und das Spiel somit zu beenden. Dabei müssen alle Spieler eine PVC-Stange zwischen ihren Beinen halten.
+                  Quidditch ist eine der jüngsten und am schnellsten wachsenden Sportarten Deutschlands. Die Münster Marauders spielen regelmäßig in der NRW-Liga und nehmen an deutschlandweiten Turnieren teil. Der Deutsche Quidditch Pokal 2022, bei dem deutschlandweit alle Teams antreten können, wurde sehr erfolgreich mit Platz 4 (bei 28 Teams) beendet. Bei der Deutschen Quidditch Meisterschaft 2022, dem Turnier der 20 besten Teams deutschlands, konnte mit dem 9. Platz sogar die Teilnahme am European Quidditch Cup Divison 2 erreicht werden, wodurch die Marauders 2023 erstmals auch auf europäischer Ebene spielen werden.
+                  Für einen ersten Eindruck wie Quidditch gespielt wird ein Regel-Video:
+                  https://www.youtube.com/watch?v=PhojXgTEs1s',
+    stamina: 2,
+    power: 2,
+    effort: 2,
+    coordination: 2,
+    physical_contact: 2 },
+  { name: 'Rhönrad',
+    description: 'Eine außergewöhnliche Sportart.
+                  Egal ob du schon Erfahrungen mit Rhönradturnen hast oder es einfach mal ausprobieren möchtest: Bei uns sind Sportbegeisterte immer gerne willkommen!
+                  Blutige Anfänger können meist sehr schnell alleine im Rad herumrollen und lernen dann erste Übungsabfolgen.
+                  Wer schon länger dabei ist oder früher schon mal Rhönrad geturnt hat, kann an sein Können anknüpfen.
+                  Es teilen sich häufig mehrere Teilnehmer ein Rad, sodass ihr meistens nur einen Teil der Trainingszeit im Rad stehen könnt. Zwischendurch ist also genug Zeit für ein Päuschen am Rand, eigenständiges Krafttraining oder auch Vorübungen an anderen Geräten.
+                  Wenn du mitmachen möchtest, solltest du Spaß an neuen Bewegungserfahrungen, Mut haben und regelmäßig teilnehmen können.
+                  Komm vorbei und du stehst garantiert Kopf!',
+    stamina: 2,
+    power: 2,
+    effort: 2,
+    coordination: 2,
+    physical_contact: 2 }
+]
 
 
 puts "Destroying db-content"
@@ -10,7 +41,9 @@ Sport.destroy_all
 puts "Done"
 
 puts "Creating sports"
-  names.each do |sport|
-    Sport.create(name: sport)
-  end
+
+sports.each do |sport|
+  Sport.create(sport)
+end
+
 puts "Done"
--- test/models/sport_test.rb
+++ test/models/sport_test.rb
@@ -4,4 +4,60 @@ class SportTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test 'sport has mandatory fields' do
+    sport = Sport.new(description: 'Testdescription', stamina: 2, power: 2, effort: 2, coordination: 2, physical_contact: 2)
+    assert_equal false, sport.save
+    sport = Sport.new(name: 'Testname', stamina: 2, power: 2, effort: 2, coordination: 2, physical_contact: 2)
+    assert_equal false, sport.save
+    sport = Sport.new(name: 'Testname', description: 'Testdescription', power: 2, effort: 2, coordination: 2, physical_contact: 2)
+    assert_equal false, sport.save
+    sport = Sport.new(name: 'Testname', description: 'Testdescription', stamina: 2, effort: 2, coordination: 2, physical_contact: 2)
+    assert_equal false, sport.save
+    sport = Sport.new(name: 'Testname', description: 'Testdescription', stamina: 2, power: 2, coordination: 2, physical_contact: 2)
+    assert_equal false, sport.save
+    sport = Sport.new(name: 'Testname', description: 'Testdescription', stamina: 2, power: 2, effort: 2, physical_contact: 2)
+    assert_equal false, sport.save
+    sport = Sport.new(name: 'Testname', description: 'Testdescription', stamina: 2, power: 2, effort: 2, coordination: 2)
+    assert_equal false, sport.save
+  end
+
+  test 'attributes have valid values' do
+    # invalid stamina
+    sport = Sport.new(name: 'Test2', description: 'Testdescription', stamina: 0, power: 2, effort: 2, coordination: 2, physical_contact: 2)
+    assert_equal false, sport.save
+    sport = Sport.new(name: 'Test2', description: 'Testdescription', stamina: 6, power: 2, effort: 2, coordination: 2, physical_contact: 2)
+    assert_equal false, sport.save
+    # invalid power
+    sport = Sport.new(name: 'Test2', description: 'Testdescription', stamina: 2, power: 6, effort: 2, coordination: 2, physical_contact: 2)
+    assert_equal false, sport.save
+    sport = Sport.new(name: 'Test2', description: 'Testdescription', stamina: 2, power: 0, effort: 2, coordination: 2, physical_contact: 2)
+    assert_equal false, sport.save
+    # invalid effort
+    sport = Sport.new(name: 'Test2', description: 'Testdescription', stamina: 0, power: 2, effort: 6, coordination: 2, physical_contact: 2)
+    assert_equal false, sport.save
+    sport = Sport.new(name: 'Test2', description: 'Testdescription', stamina: 6, power: 2, effort: 0, coordination: 2, physical_contact: 2)
+    assert_equal false, sport.save
+    # invalid coordinati
+    sport = Sport.new(name: 'Test2', description: 'Testdescription', stamina: 0, power: 2, effort: 2, coordination: 6, physical_contact: 2)
+    assert_equal false, sport.save
+    sport = Sport.new(name: 'Test2', description: 'Testdescription', stamina: 6, power: 2, effort: 2, coordination: 0, physical_contact: 2)
+    assert_equal false, sport.save
+    # invalid physical_c
+    sport = Sport.new(name: 'Test2', description: 'Testdescription', stamina: 0, power: 2, effort: 2, coordination: 2, physical_contact: 6)
+    assert_equal false, sport.save
+    sport = Sport.new(name: 'Test2', description: 'Testdescription', stamina: 6, power: 2, effort: 2, coordination: 2, physical_contact: 0)
+    assert_equal false, sport.save
+  end
+
+  test 'name is unique' do
+    Sport.create(name: 'Test1', description: 'Testdescription', stamina: 2, power: 2, effort: 2, coordination: 2, physical_contact: 2)
+    sport = Sport.new(name: 'Test1', description: 'Testdescription', stamina: 0, power: 2, effort: 6, coordination: 2, physical_contact: 2)
+    assert_equal false, sport.save
+  end
+
+  test 'description is not > 1000 characters' do
+    sport = Sport.new(name: 'Test1', description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Na', stamina: 0, power: 2, effort: 6, coordination: 2, physical_contact: 2)
+    assert_equal false, sport.save
+  end
+
 end
